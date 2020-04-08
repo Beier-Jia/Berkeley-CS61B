c443cd103f25b0396503de4b851b8e5a19b00e5d
Beier-Jia <61725797+Beier-Jia@users.noreply.github.com>
2020-04-08 14:30:07 +0800

    Add files via upload

---
+++ Lab 3/Flik.java
@@ -0,0 +1,9 @@
+/** An Integer tester created by Flik Enterprises. */
+public class Flik {
+    public static boolean isSameNumber(int a, int b) {
+        return a == b;
+    }
+}
+
+/** The JVM is caching Integer values.
+ * == only works for numbers between -128 and 127. */
