c95c2ef53c143294b629698d71922c08658468a3
Beier-Jia <61725797+Beier-Jia@users.noreply.github.com>
2020-04-10 16:15:10 +0800

    Update Deque.java

--- Project 1B/Deque.java
+++ Project 1B/Deque.java
@@ -1,7 +1,7 @@
 public interface Deque<Item> {
     public void addFirst(Item x);
     public void addLast(Item x);
-    public default boolean isEmpty() {
+    default public boolean isEmpty() {
         return size() == 0;
     }
     public int size();
