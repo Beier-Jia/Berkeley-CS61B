e32cbed7983468c6fbce91b633d3c3cd4a7f9c54
Beier-Jia <61725797+Beier-Jia@users.noreply.github.com>
2020-04-13 10:35:54 +0800

    Update Deque.java

--- Project 1B/Deque.java
+++ Project 1B/Deque.java
@@ -1,13 +1,13 @@
 public interface Deque<Item> {
-    public void addFirst(Item x);
-    public void addLast(Item x);
-    default public boolean isEmpty() {
+    void addFirst(Item x);
+    void addLast(Item x);
+    default boolean isEmpty() {
         return size() == 0;
     }
-    public int size();
-    public void printDeque();
-    public Item removeFirst();
-    public Item removeLast();
-    public Item get(int index);
+    int size();
+    void printDeque();
+    Item removeFirst();
+    Item removeLast();
+    Item get(int index);
 }
 
